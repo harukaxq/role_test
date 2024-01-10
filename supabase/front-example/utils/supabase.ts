1b1f111be96aed42d2dfdf450fcb52957c1e811d
harukaxq <h.tamura@cibo.design>
2024-01-10 18:51:48 +0900

    change dir

--- supabase/front-example/utils/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClientComponentClient({
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-}
-)
-export default supabase;
